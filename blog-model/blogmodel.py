f70f3764331802c868f350a88dfc629c5078915c
Dave Campbell <davecampbell13@gmail.com>
2015-04-27 14:04:02 -0600

    working functions for blog model

--- blog-model/blogmodel.py
+++ blog-model/blogmodel.py
@@ -28,7 +28,6 @@ D - Destroy
 
 import sqlite3
 
-
 class BlogModel():
     def __init__(self,db_file):
         self.db_file = db_file
@@ -37,29 +36,63 @@ class BlogModel():
         self.post_text = None
 
     def open(self):
-        "open sqlite3 db connection"
-        pass
+        #"open sqlite3 db connection"
+        # Creates or opens a file called mydb with a SQLite3 DB
+        self.db = sqlite3.connect(self.db_file)
 
     def close(self):
-        "close the connection to sqlite3"
-        pass
+        #"close the connection to sqlite3"
+        self.db.close()
 
     def create_table(self):
         #create the table
-        pass
+        # Get a cursor object
+        cursor = self.db.cursor()
+        cursor.execute('''
+            CREATE TABLE blogcontent(id INTEGER PRIMARY KEY, post_name TEXT, post_text TEXT)
+        ''')
+        self.db.commit()
 
     def create(self, post_name, post_text):
         #create a new row with data that you pass in
-        pass
+        cursor = self.db.cursor()
+
+        # Insert user 1
+        cursor.execute('''INSERT INTO blogcontent(post_name, post_text )
+                  VALUES(?,?)''', (post_name,post_text))
+        print('First user inserted')
+
+        self.db.commit()
 
     def read(self,id):
         # "search for id, and return post_name and post_text as a string"
-        pass
+        cursor = self.db.cursor()
+
+        cursor.execute('''SELECT post_name, post_text FROM blogcontent WHERE id=?''', (id,))
+        post = cursor.fetchone()
+        print "Title:\n" + post[0] + "\n"
+        print "Post Content:\n" + post[1]
 
     def update(self, id, post_name, post_text):
-        pass
+        cursor = self.db.cursor()
+
         # "search for id, and set a new post_name and post_text"
+        cursor.execute('''UPDATE blogcontent SET post_name = ?, post_text = ? WHERE id=?''',
+        (post_name, post_text, id))
+
+        self.db.commit()
 
     def destroy(self,id):
         #"search for id, and delete that row"
-        pass
+        cursor = self.db.cursor()
+
+        cursor.execute('''DELETE FROM blogcontent WHERE id = ? ''', (id,))
+         
+        self.db.commit()
+
+blog = BlogModel("database.db")
+blog.open()
+#blog.create("Second Post", "All the words.")
+#blog.read("1")
+#blog.update("1","Firstest Post", "New content for things.")
+blog.destroy("3")
